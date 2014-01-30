7373b9ab8a0d9f9f6cb8a2bc9962c6f5e9722ee1
Jiri Slaby <jslaby@suse.cz>
2014-01-30 11:04:10 +0100

    kgr: sched.h, introduce kgr_task_safe helper
    To be used from some kthreads.
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@redhat.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2970,6 +2970,15 @@ static inline void mm_update_next_owner(struct mm_struct *mm)
 }
 #endif /* CONFIG_MEMCG */
 
+#if IS_ENABLED(CONFIG_KGRAFT)
+static inline void kgr_task_safe(struct task_struct *p)
+{
+	clear_bit(0, &task_thread_info(p)->kgr_in_progress);
+}
+#else
+static inline void kgr_task_safe(struct task_struct *p) { }
+#endif /* IS_ENABLED(CONFIG_KGRAFT) */
+
 static inline unsigned long task_rlimit(const struct task_struct *tsk,
 		unsigned int limit)
 {
