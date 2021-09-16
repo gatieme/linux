49f6f0c29d3d4a3ba2942c645ca846c2475398b4
Roman Gushchin <guro@fb.com>
2021-09-16 09:24:47 -0700

    bpf: sched: add convenient helpers to identify sched entities
    This patch adds 3 helpers useful for dealing with sched entities:
      u64 bpf_sched_entity_to_tgidpid(struct sched_entity *se);
      u64 bpf_sched_entity_to_cgrpid(struct sched_entity *se);
      long bpf_sched_entity_belongs_to_cgrp(struct sched_entity *se, u64 cgrpid);
    
    Sched entity is a basic structure used by the scheduler to represent
    schedulable objects: tasks and cgroups (if CONFIG_FAIR_GROUP_SCHED
    is enabled). It will be passed as an argument to many bpf hooks, so
    scheduler bpf programs need a convenient way to deal with it.
    
    bpf_sched_entity_to_tgidpid() and bpf_sched_entity_to_cgrpid() are
    useful to identify a sched entity in userspace terms (pid, tgid and
    cgroup id). bpf_sched_entity_belongs_to_cgrp() allows to check whether
    a sched entity belongs to sub-tree of a cgroup. It allows to write
    cgroup-specific scheduler policies even without enabling the cgroup
    cpu controller.
    
    Signed-off-by: Roman Gushchin <guro@fb.com>
    Signed-off-by: Cheng Jian <cj.chengjian@huawei.com>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -4879,6 +4879,26 @@ union bpf_attr {
  *		Get the struct pt_regs associated with **task**.
  *	Return
  *		A pointer to struct pt_regs.
+ *
+ * u64 bpf_sched_entity_to_tgidpid(struct sched_entity *se)
+ *	Description
+ *		Return task's encoded tgid and pid if the sched entity is a task.
+ *	Return
+ *		Tgid and pid encoded as tgid << 32 \| pid, if *se* is a task. (u64)-1 otherwise.
+ *
+ * u64 bpf_sched_entity_to_cgrpid(struct sched_entity *se)
+ *	Description
+ *		Return cgroup id if the given sched entity is a cgroup.
+ *	Return
+ *		Cgroup id, if *se* is a cgroup. (u64)-1 otherwise.
+ *
+ * long bpf_sched_entity_belongs_to_cgrp(struct sched_entity *se, u64 cgrpid)
+ *	Description
+ *		Checks whether the sched entity belongs to a cgroup or
+ *		it's sub-tree. It doesn't require a cgroup CPU controller
+ *		to be enabled.
+ *	Return
+ *		1 if the sched entity belongs to a cgroup, 0 otherwise.
  */
 #define __BPF_FUNC_MAPPER(FN)		\
 	FN(unspec),			\
@@ -5057,6 +5077,9 @@ union bpf_attr {
 	FN(get_func_ip),		\
 	FN(get_attach_cookie),		\
 	FN(task_pt_regs),		\
+	FN(sched_entity_to_tgidpid),	\
+	FN(sched_entity_to_cgrpid),	\
+	FN(sched_entity_belongs_to_cgrp),	\
 	/* */
 
 /* integer value in 'imm' field of BPF_CALL instruction selects which helper
